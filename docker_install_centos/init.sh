801313970125a12f479cdcd1de9e354ac1a8b261
iitenkida7 <t-ishii@sencorp.co.jp>
2018-04-12 18:58:57 +0900

    runging docker daemon

--- docker_install_centos/init.sh
+++ docker_install_centos/init.sh
@@ -9,9 +9,12 @@ sudo yum makecache fast
 sudo yum -y  install docker-ce
 sudo usermod -aG docker ${USER}
 
-
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chown 755 /usr/local/bin/docker-compose
+
+
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # 成功してるかな−
 docker version
