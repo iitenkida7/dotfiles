e732345793235403c69c5a1c5675614a50fbbcd2
ishii <ishii@kanda51.8122.jp>
2019-05-14 18:29:32 +0900

    update install apps

--- homebrew/appInstallList.sh
+++ homebrew/appInstallList.sh
@@ -3,27 +3,12 @@
 #cask
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 #cask list
-brew cask install iterm2 
-brew cask install remote-desktop-connection 
-brew cask install virtualbox
 brew cask install clipy
-brew-cask install skitch
-brew-cask install evernote
-
-#maindMap
-brew cask install  xmind
-#cask list end
-
 brew cask cleanup
-
 brew install tmux
 brew install wget
 brew install vim
-brew install zsh
 brew install jq
 brew install curl
-brew install docker boot2docker
-brew install  mysql
-brew install  nkf
-#brew cleanup
-#brew cask clenup
+brew install nkf
+brew install telnet
